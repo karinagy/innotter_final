d72635a55b1c4311347b38b0e0b04b76d6123029
Karina Rabcovskaya <karina6918538@gmail.com>
2023-02-09 19:08:14 +0300

    Fix permissions

--- core/permissions.py
+++ core/permissions.py
@@ -5,20 +5,14 @@ from core.enums.user_enums import Roles
 
 class IsUser(BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        return Roles.USER.value in user.role
+        return Roles.USER in request.user.role
 
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        return Roles.MODERATOR.value in user.role
+        return Roles.MODERATOR in request.user.role
 
 
 class IsAdminOrModerator(BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        try:
-            return Roles.MODERATOR.value in user.role or Roles.ADMIN.value in user.role
-        except Exception as e:
-            return False
+        return Roles.MODERATOR in request.user.role or Roles.ADMIN in request.user.role
